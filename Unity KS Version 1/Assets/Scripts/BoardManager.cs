9b9dc392a84ce9783fcc89f1647d0cf2fab0d873
jpfranco123 <jfranco1@3330D-132413-M.local>
2017-03-21 13:45:50 +1100

    First Version Upload

---
+++ Unity KS Version 1/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+
+// This Script (a component of Game Manager) Initializes the Borad (i.e. screen).
+public class BoardManager : MonoBehaviour {
+
+	//66. Delete Count Class if is not used.
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count(int min, int max)
+		{
+			minimum=min;
+			maximum=max;
+		}
+	}
+
+	//Resoultion width and Height
+	public int resolutionWidth = 800;
+	public int resolutionHeight = 600;
+
+	//Number of Columns and rows of the grid (the possible positions of the items).
+	public int columns = 8;
+	public int rows = 6;
+
+	//Prefab of the item interface configuration
+	public static GameObject KSItemPrefab;
+
+	//A canvas where all the board is going to be placed
+	private GameObject canvas;
+
+	//The item radius. This is used to avoid superposition of items.
+	public float KSItemRadius = 1.5f;
+
+	//The possible positions of the items;
+	private List <Vector3> gridPositions = new List<Vector3> ();
+
+	//Weights and value vectors for this trial. CURRENTLY SET UP TO ALLOW ONLY INTEGERS.
+	//ws and vs must be of the same length
+	private int[] ws;
+	private int[] vs;
+
+
+	//This Initializes the GridPositions
+	void InitialiseList()
+	{
+		gridPositions.Clear ();
+
+		for(int x=1;x<columns+1;x++)
+		{
+			for ( int y =1; y<rows+1;y++)
+			{	
+				float xUnit =(float) (resolutionWidth / 100)/columns;
+				float yUnit =(float) (resolutionHeight / 100)/rows;
+				gridPositions.Add(new Vector3(x*xUnit,y*yUnit,0f));
+
+			}
+		}
+	}
+
+	//Randomizes YES/NO button positions (left or right) and allocates corresponding script to save the correspondent answer.
+	void RandomizeButtons(){
+		Button btnLeft = GameObject.Find("LEFTbutton").GetComponent<Button>();
+		Button btnRight = GameObject.Find("RIGHTbutton").GetComponent<Button>();
+
+		int randomYes=Random.Range(0,2);
+
+		if (randomYes == 1) {
+			btnLeft.GetComponentInChildren<Text>().text = "No";
+			btnRight.GetComponentInChildren<Text>().text = "Yes";
+			btnLeft.onClick.AddListener(()=>GameManager.changeToNextTrial(2,0));
+			btnRight.onClick.AddListener(()=>GameManager.changeToNextTrial(2,1));
+		} else {
+			btnLeft.GetComponentInChildren<Text>().text = "Yes";
+			btnRight.GetComponentInChildren<Text>().text = "No";
+			btnLeft.onClick.AddListener(()=>GameManager.changeToNextTrial(2,1));
+			btnRight.onClick.AddListener(()=>GameManager.changeToNextTrial(2,0));
+		}
+	}
+
+	//Initializes the instance for this trial:
+	//1. Sets the question string using the instance (from the .txt files)
+	//2. The weight and value vectors are uploaded
+	//3. The instance prefab is uploaded
+	void setKSInstance(){
+		int randInstance = GameManager.instanceRandomization[GameManager.trial];
+
+		Text Quest = GameObject.Find("Question").GetComponent<Text>();
+
+		String question = "Can you get at least $" + GameManager.ksinstances[randInstance].profit + " using at most " + GameManager.ksinstances[randInstance].capacity +"kg?";
+
+		Quest.text = question;
+
+		//123 Currently saving the new items on KSItems so they are randomized as before
+		//66 Carefull: Make sure that KSItems is erased and started in each trial. Static?
+
+		ws = GameManager.ksinstances [randInstance].weights;
+		vs = GameManager.ksinstances [randInstance].values;
+
+		KSItemPrefab = (GameObject)Resources.Load ("KSItem");
+
+	}
+
+	//Intsantiates an Item
+	void generateItem(int itemNumber ,Vector3 randomPosition){
+
+		GameObject instance = Instantiate (KSItemPrefab, randomPosition, Quaternion.identity) as GameObject;
+
+		canvas=GameObject.Find("Canvas");
+		instance.transform.SetParent (canvas.GetComponent<Transform> (),false);
+
+		//Setting the position in a separate line is importatant in order to set it according to global coordinates.
+		instance.transform.position = randomPosition;
+
+		instance.GetComponentInChildren<Text>().text = ws[itemNumber]+ "Kg \n $" + vs[itemNumber];
+
+	}
+
+
+	//Retunrs a random position from the grid and removes the item from the list.
+	Vector3 RandomPosition()
+	{
+		int randomIndex=Random.Range(0,gridPositions.Count);
+		Vector3 randomPosition = gridPositions[randomIndex];
+		gridPositions.RemoveAt(randomIndex);
+		return randomPosition;
+	}
+
+	//Places all the objects from the instance (ws,vs) on the canvas. 
+	// Returns TRUE if all items where positioned, FALSE otherwise.
+	bool LayoutObjectAtRandom()
+	{
+		int objectCount =ws.Length;
+		for(int i=0; i < objectCount;i++)
+		{
+			int objectPositioned = 0;
+			while (objectPositioned == 0) 
+			{
+				if (gridPositions.Count > 0) {
+					Vector3 randomPosition = RandomPosition ();
+
+					//66: SImplify expression
+					if (objectOverlapsQ (randomPosition)) {
+					
+					} else {
+						generateItem (i, randomPosition);
+						objectPositioned = 1;
+					}
+				}
+				else{
+					//66. If not all are able to be located try several times and if still not: Show an error
+					Debug.Log ("Not enough space to place all items");
+					return false;
+				}
+			}
+
+		}
+		return true;
+	}
+
+	//Macro function that initializes the Board
+	public void SetupScene()
+	{
+		InitialiseList();
+		RandomizeButtons ();
+		setKSInstance ();
+
+		//66 Generate: if the bool return by the following function is false, then retry again.
+		LayoutObjectAtRandom();
+
+	}
+
+	//Checks if positioning an item in the new position generates an overlap. Assuming the new item has a radius of KSITemRadius.
+	//Returns: TRUE if there is an overlap. FALSE Otherwise.
+	bool objectOverlapsQ(Vector3 pos)
+	{
+		//If physics could be started before update we could use the following easier function:
+		//bool overlap = Physics2D.IsTouchingLayers(newObject.GetComponent<Collider2D>());
+
+		bool overlap = Physics2D.OverlapCircle(pos,KSItemRadius);
+		return overlap;
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Unity KS Version 1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,225 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour {
+
+	//Game Manager: It is a singleton (i.e. it is always one and the same it is nor destroyed nor duplicated)
+	public static GameManager instance=null;
+
+	//The reference to the script managing the board (interface/canvas).
+	public BoardManager boardScript;
+
+	//Current Scene
+	private static int escena;
+
+	//Time spent on this scene
+	public static float tiempo;
+
+	//Current trial initialization
+	public static int trial = 0;
+
+
+	//Modifiable Variables:
+	//Interperiod Time
+	public static float timeRest1=5;
+
+	//Time given for each trial
+	public static float timeTrial=10;
+
+	//Total number of trials
+	private static int numberOfTrials = 30;
+
+	//Number of instance file to be considered. From i1.txt to i_.txt..
+	public static int numberOfInstances = 3;
+
+	//The order of the instances to be presented
+	public static int[] instanceRandomization;
+
+	//Timer width
+	private static float timerWidth =300;
+
+	//This is the string that will be used as the file name where the data is stored. Currently the date-time is used.
+	private static string participantID = @System.DateTime.Now.ToString("dd MMMM, yyyy, HH-mm");
+
+
+
+
+
+	//A structure that contains the parameters of each instance
+	public struct KSInstance
+	{
+		public int capacity;
+		public int profit;
+
+		public int[] weights;
+		public int[] values;
+	}
+
+	//An array of all the instances to be uploaded form .txt files.
+	public static KSInstance[] ksinstances = new KSInstance[numberOfInstances];
+
+
+
+
+	// Use this for initialization
+	void Awake () {
+
+		//Makes the Gama manager a Singleton
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+
+
+		//Initializes the game
+		boardScript = GetComponent<BoardManager> ();
+
+		//66: Relocate to to InitGame
+		Scene scene = SceneManager.GetActiveScene();
+		escena = scene.buildIndex;
+		InitGame();
+
+	}
+
+
+	//Initializes the scene. One scene is setup, other is trial, other is Break....
+	//escena = 		0->Setup	1->Trial	2->RestPeriod
+	void InitGame(){
+
+		if (escena == 0) {
+			loadKPInstance ();
+			RandomizeKSInstances ();
+			SceneManager.LoadScene(1);
+		}
+		else if ( escena == 1) {
+			boardScript.SetupScene ();
+			tiempo = timeTrial;
+			trial++;
+		}
+		else if (escena == 2) {
+			tiempo = timeRest1; 
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		startTimer ();
+	}
+		
+	//Saves the data of a trial to a .txt file with the participants ID as filename using StreamWriter.
+	//If the file doesn't exist it creates it. Otherwise it adds on lines to the existing file.
+	//Each line in the File has the following structure: "trial;answer;timeSpent".
+	public static void save(int answer, float timeSpent) {
+
+		string dataTrialText = trial + ";" + answer + ";" + timeSpent; 
+
+		string[] lines = {dataTrialText};
+		string folderPathSave = @"/Users/jfranco1/Desktop/Unity Projects/knapsack4Imaging/DATA/";
+
+		//This location can be used by unity to save a file if u open the game in any platform/computer:      Application.persistentDataPath;
+
+		using (StreamWriter outputFile = new StreamWriter(folderPathSave + participantID,true)) {
+			foreach (string line in lines)
+				outputFile.WriteLine(line);
+		} 
+
+		//Options of streamwriter include: Write, WriteLine, WriteAsync, WriteLineAsync
+	}
+
+	/*
+	 * Loads all of the instances to be uploaded form .txt files. Example of input file:
+	 * Name of the file: i3.txt 
+	 * Structure of each file is the following:
+	 * weights:[2,5,8,10,11,12]
+	 * values:[10,8,3,9,1,4]
+	 * capacity:15
+	 * profit:16
+	 * 
+	 * The instances are stored as ksinstances structures in the array of structures: ksinstances
+	 */
+	public static void loadKPInstance(){
+		string folderPathLoad = @"/Users/jfranco1/Desktop/Unity Projects/knapsack4Imaging/KPInstances/";
+		int linesInEachKPInstance = 4;
+
+		for (int k = 1; k <= numberOfInstances; k++) {
+			
+			string[] KPInstanceText = new string[linesInEachKPInstance];
+			try {   // Open the text file using a stream reader.
+				using (StreamReader sr = new StreamReader (folderPathLoad + "i"+ k +".txt")) {
+					for (int i = 0; i < linesInEachKPInstance; i++) {
+						string line = sr.ReadLine ();
+						string[] dataInLine = line.Split (':');
+						KPInstanceText [i] = dataInLine [1];
+					}
+					// Read the stream to a string, and write the string to the console.
+					//String line = sr.ReadToEnd();
+				}
+			} catch (Exception e) {
+				Debug.Log ("The file could not be read:");
+				Debug.Log (e.Message);
+			}
+
+			int textLineN = 0;
+			ksinstances[k-1].weights = Array.ConvertAll (KPInstanceText [textLineN].Substring (1, KPInstanceText [textLineN].Length - 2).Split (','), int.Parse);
+
+			textLineN = 1;
+			ksinstances[k-1].values = Array.ConvertAll (KPInstanceText [textLineN].Substring (1, KPInstanceText [textLineN].Length - 2).Split (','), int.Parse);
+
+			textLineN = 2;
+			ksinstances[k-1].capacity = int.Parse (KPInstanceText [textLineN]);
+
+			textLineN = 3;
+			ksinstances[k-1].profit = int.Parse (KPInstanceText [textLineN]);
+
+		}
+			
+	}
+
+
+	//Randomizes the sequence of Instances to be shown to the participant adn stores it in: instanceRandomization
+	void RandomizeKSInstances(){
+		instanceRandomization = new int[numberOfTrials];
+		for (int i = 0; i < numberOfTrials; i++) {
+				instanceRandomization[i] = Random.Range(0,numberOfInstances);
+		}
+
+	}
+
+	//Saves and Changes to the next trial
+	public static void changeToNextTrial(int newKPInstance, int answer){
+		save (answer, timeTrial-tiempo);
+		SceneManager.LoadScene(newKPInstance);
+	}
+
+	//Updates the timer (including the graphical representation)
+	//If time runs out in the trial or the break scene. It switches to the next scene. 
+	void startTimer(){
+		tiempo -= Time.deltaTime;
+		//Debug.Log (tiempo);
+
+		RectTransform timer = GameObject.Find ("Timer").GetComponent<RectTransform>();
+
+		timer.sizeDelta = new Vector2(timerWidth*(tiempo/timeTrial), timer.rect.height);
+
+		if(tiempo < 0)
+		{	
+			if (escena == 1) {
+				save (2, timeTrial);
+			}
+
+			int nuevaEscena = (escena == 1) ? 2 : 1;
+			SceneManager.LoadScene(nuevaEscena);
+		}
+	}
+
+
+}
