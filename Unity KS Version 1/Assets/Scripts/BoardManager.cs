220f714a69ecd6f0645f90f5df15fe1e034884a9
jpfranco123 <jfranco1@3330D-132413-M.local>
2017-04-10 18:41:49 +1000

    Version 1.05.1
    -Implementation of the interface with all the overlaping-items-bug
    corrected.

--- Unity KS Version 1/Assets/Scripts/BoardManager.cs
+++ Unity KS Version 1/Assets/Scripts/BoardManager.cs
@@ -49,15 +49,26 @@ public class BoardManager : MonoBehaviour {
 	//Should the key be working?
 	public static bool keysON = false;
 
-
+	//These variables shouldn't be modified. They just state that the area of the value part of the item and the weight part are assumed to be 1.
 	private static float minAreaBill = 1f;
-
 	private static float minAreaWeight = 1f;
 
-	private static int totalAreaBill = 7;
+	//The total area of all the items. Separated by the value part and the weighy part. A good initialization for this variables is the number of items plus 1.
+	private static int totalAreaBill = 8;
+	private static int totalAreaWeight = 8;
 
-	private static int totalAreaWeight = 7;
 
+	//Structure with the relvant parameters of an item.
+	private struct Item
+	{
+		public GameObject gameItem;
+		public Vector2 coordValue1;
+		public Vector2 coordValue2;
+		public Vector2 coordWeight1;
+		public Vector2 coordWeight2;
+	}
+
+	//An array of all the instances to be uploaded form .txt files.
 
 
 
@@ -126,7 +137,6 @@ public class BoardManager : MonoBehaviour {
 
 		KSItemPrefab = (GameObject)Resources.Load ("KSItem3");
 
-
 	}
 
 	//Shows the question on the screen
@@ -137,7 +147,7 @@ public class BoardManager : MonoBehaviour {
 
 
 	//Intsantiates an Item
-	void generateItem(int itemNumber ,Vector3 randomPosition){
+	Item generateItem(int itemNumber ,Vector3 randomPosition){
 
 		GameObject instance = Instantiate (KSItemPrefab, randomPosition, Quaternion.identity) as GameObject;
 
@@ -153,7 +163,12 @@ public class BoardManager : MonoBehaviour {
 		bill.GetComponentInChildren<Text>().text = "$" + vs[itemNumber];
 
 		GameObject weight = instance.transform.Find("Weight").gameObject;
-		weight.GetComponentInChildren<Text>().text = ws[itemNumber]+ "Kg";
+		if (ws [itemNumber] < 10) {
+			weight.GetComponentInChildren<Text>().text = "" + ws[itemNumber]+ "kg";
+		} else {
+			weight.GetComponentInChildren<Text>().text = ws[itemNumber]+ "kg";
+		}
+
 
 
 		// This calculates area accrding to approach 1
@@ -178,7 +193,31 @@ public class BoardManager : MonoBehaviour {
 		float areaItem2 = adjustmentWeight + (totalAreaWeight - ws.Length * adjustmentWeight) * ws [itemNumber] / ws.Sum ();
 		float scale2 = Convert.ToSingle (Math.Sqrt (areaItem2) - 1);
 		weight.transform.localScale += new Vector3 (scale2, scale2, 0);
+			
 
+		float weightH = weight.GetComponent<BoxCollider2D> ().size.y;
+		float weightW = weight.GetComponent<BoxCollider2D> ().size.x;
+		float valueH = bill.GetComponent<BoxCollider2D> ().size.y;
+		float valueW = bill.GetComponent<BoxCollider2D> ().size.x;
+
+
+		Item itemInstance = new Item();
+		itemInstance.gameItem=instance;
+		itemInstance.coordValue1=new Vector2(-valueW*(1+scale1)/2,0);
+		itemInstance.coordValue2=new Vector2(valueW*(1+scale1)/2,valueH*(1+scale1));
+		itemInstance.coordWeight1=new Vector2(-weightW*(1+scale2)/2,0);
+		itemInstance.coordWeight2=new Vector2(weightW*(1+scale2)/2,-weightH*(1+scale2));
+
+		//.GetComponent<BoxCollider2D>().size.y
+
+		return(itemInstance);
+
+
+	}
+
+	void placeItem(Item itemToLocate, Vector3 position){
+		//Setting the position in a separate line is importatant in order to set it according to global coordinates.
+		itemToLocate.gameItem.transform.position = position;
 
 	}
 
@@ -201,19 +240,16 @@ public class BoardManager : MonoBehaviour {
 		for(int i=0; i < objectCount;i++)
 		{
 			int objectPositioned = 0;
+			Item itemToLocate = generateItem (i, new Vector3 (-1000,-1000,-1000));//66: Change to different Layer?
 			while (objectPositioned == 0) 
 			{
 				if (gridPositions.Count > 0) {
 					Vector3 randomPosition = RandomPosition ();
 
-					if (!objectOverlapsQ (randomPosition)) {
-						generateItem (i, randomPosition);
+					if (!objectOverlapsQ (randomPosition,itemToLocate)) {
+						placeItem (itemToLocate, randomPosition);
 						objectPositioned = 1;
 					} 
-//					else {
-//						generateItem (i, randomPosition);
-//						objectPositioned = 1;
-//					}
 				}
 				else{
 					//Debug.Log ("Not enough space to place all items");
@@ -277,14 +313,13 @@ public class BoardManager : MonoBehaviour {
 
 	//Checks if positioning an item in the new position generates an overlap. Assuming the new item has a radius of KSITemRadius.
 	//Returns: TRUE if there is an overlap. FALSE Otherwise.
-	bool objectOverlapsQ(Vector3 pos)
+	bool objectOverlapsQ(Vector3 pos, Item item)
 	{
-		//If physics could be started before update we could use the following easier function:
-		//bool overlap = Physics2D.IsTouchingLayers(newObject.GetComponent<Collider2D>());
+		Vector2 posxy = new Vector3 (pos.x, pos.y);
+		bool overlapValue = Physics2D.OverlapArea (item.coordValue1+posxy, item.coordValue2+posxy);
+		bool overlapWeight = Physics2D.OverlapArea (item.coordWeight1+posxy, item.coordWeight2+posxy);
 
-		bool overlap = Physics2D.OverlapCircle(pos,KSItemRadius);
-		return overlap;
-
+		return overlapValue || overlapWeight;
 	}
 
 	public void updateTimer(){
